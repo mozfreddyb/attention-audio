07a93bef80ec49d58e460a71a990fe2466f480b2
Frederik B <fbraun+gh@mozilla.com>
2014-06-17 16:55:05 +0200

    init

---
+++ main.js
@@ -0,0 +1,10 @@
+addEventListener("DOMContentLoaded", function() {
+  var an = document.getElementById("attentionNow");
+  an.addEventListener("click", function() {
+    window.open("attention.html", "attention")
+  });
+  var al = document.getElementById("attention10secs");
+  al.addEventListener("click", function() {
+    setTimeout(function() { window.open("attention.html", "attention") }, 10000);
+  });
+});
