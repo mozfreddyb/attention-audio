70422262caac6901186fad1ef22dcadb611ac642
Frederik B <fbraun+gh@mozilla.com>
2014-07-02 11:38:32 +0200

    fixed JS to use the real attention feature

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 addEventListener("DOMContentLoaded", function() {
   var an = document.getElementById("attentionNow");
   an.addEventListener("click", function() {
-    window.open("attention.html", "attention")
+    window.open("attention.html", "", "attention")
   });
   var al = document.getElementById("attention10secs");
   al.addEventListener("click", function() {
-    setTimeout(function() { window.open("attention.html", "attention") }, 10000);
+    setTimeout(function() { window.open("attention.html", "", "attention") }, 10000);
   });
 });
