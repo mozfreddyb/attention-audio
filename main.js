c1a4a834e4b15cf098e1f2d10bb49169f528e129
Frederik B <fbraun+gh@mozilla.com>
2014-07-03 12:29:45 +0200

    yolo

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 addEventListener("DOMContentLoaded", function() {
+  var np = document.getElementById("normalPopup");
+  np.addEventListener("click", function() {
+     popups.push(window.open("popup.html"));
+  });
   var an = document.getElementById("attentionNow");
   an.addEventListener("click", function() {
-    window.open("attention.html", "", "attention")
+    popups.push(window.open("attention.html", "", "attention"))
   });
+
   var al = document.getElementById("attention10secs");
   al.addEventListener("click", function() {
-    setTimeout(function() { window.open("attention.html", "", "attention") }, 10000);
+    setTimeout(function() { popups.push(window.open("attention.html", "", "attention")) }, 10000);
+  });
+
+  var btnplaynormal = document.getElementById("playnormal").addEventListener("click", function (e) {
+    setTimeout(function() {document.getElementById("normalaudio").play() }, 5000);
+  });
+
+  var btnplayringer = document.getElementById("playr").addEventListener("click", function (e) {
+    setTimeout(function() {document.getElementById("ringer").play() }, 5000);
+  });
+
+  var btnplaytelephony = document.getElementById("playtp").addEventListener("click", function (e) {
+    setTimeout(function() { document.getElementById("telephony").play() }, 5000)
   });
 });
+window.onmessage= function(e) {
+  console.log(e);
+}
+var popups = [];
